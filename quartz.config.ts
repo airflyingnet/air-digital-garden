f446b4b40b421b90a0fff746afec98a782867443
airflyingnet <airflyingnet@gmail.com>
2026-05-13 23:16:08 +0900

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -17,7 +17,7 @@ const config: QuartzConfig = {
     },
     locale: "en-US",
     baseUrl: "quartz.jzhao.xyz",
-    ignorePatterns: ["private", "templates", ".obsidian", "air","categories","attachments"],
+    ignorePatterns: ["private", "templates", ".obsidian", "air","categories","attachments", "references/templates"],
     defaultDateType: "modified",
     theme: {
       fontOrigin: "googleFonts",
